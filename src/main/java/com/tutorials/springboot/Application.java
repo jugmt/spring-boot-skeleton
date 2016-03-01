a968354d914b9872df1c17d3a719efbb325676eb
Joseph Masini <joseph.masini1@gmail.com>
2016-03-01 20:39:37 +0100

    Initial commit of spring boot skeleton.

---
+++ src/main/java/com/tutorials/springboot/Application.java
@@ -0,0 +1,18 @@
+package com.tutorials.springboot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * Spring Boot Application main class.
+ *
+ * @author Joseph Masini
+ */
+@SpringBootApplication
+public class Application {
+
+    public static void main(final String[] args) {
+        SpringApplication.run(Application.class, args);
+    }
+
+}
